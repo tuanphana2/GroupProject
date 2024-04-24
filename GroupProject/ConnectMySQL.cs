ea1fd6b1c16e3b923537cd7b5480b48a58cb14b8
PQT <tuanphana202@gmail.com>
2024-04-24 17:02:33 +0700

    masterPage

---
+++ GroupProject/ConnectMySQL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace GroupProject
+{
+    public class ConnectMySQL
+    {
+        static void Main()
+        {
+            string connectionString = "server=localhost;user id=username;password=password;database=SI Payroll DB";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Kết nối đến MySQL thành công!");
+
+                // Thực hiện các thao tác với cơ sở dữ liệu ở đây...
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
